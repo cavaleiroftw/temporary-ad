6794496ef34130fa4f41cab355742aeb07b6626e
Cavaleiro <118493715+caavaleiro@users.noreply.github.com>
2022-12-26 11:41:33 -0300

    Playlist tweaks

--- src/main/java/work/cavaleiro/java/system/AdSystem.java
+++ src/main/java/work/cavaleiro/java/system/AdSystem.java
@@ -3,29 +3,25 @@ package work.cavaleiro.java.system;
 import org.bukkit.Bukkit;
 import work.cavaleiro.java.Client;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AdSystem {
 
     private static final int count = 120;
-    private static List<String> messages = new ArrayList<>();
-
-    static {
-        messages.add(Client.getInstance().getConfig().getString("system.ad"));
-    }
+    private static List<String> messages = Arrays.asList(Client.getInstance().getConfig().getStringList("system.ad"));
     
     public static void initialize() {
         Bukkit.getScheduler().scheduleSyncRepeatingTask(Client.getInstance(), new Runnable() {
             int timer = count;
             int index = 1;
+            
             public void run() {
                 timer -= 1;
 
-                if (timer == 0) {
+                if (timer == 0)
                     Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(messages.get(index)));
-                }
-
+                
                 index += 1;
                 if (index >= messages.size())
                     index = 1;
